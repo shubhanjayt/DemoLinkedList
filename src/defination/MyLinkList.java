94ccf55f610e9186c353b1c94ed3242845828658
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-01-24 16:05:38 +0530

    create the node class

--- src/defination/MyLinkList.java
+++ src/defination/MyLinkList.java
@@ -1,5 +1,35 @@
 package defination;
 
-public class MyLinkList<E> {
+import adt.LinkedListADT;
 
+public class MyLinkList<E> implements LinkedListADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove(E item) {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    private static class Node<E> {
+
+    }
 }
